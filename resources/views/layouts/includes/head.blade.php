c5a11ea251f53909c2dfa2be5ca459cda45a24b9
ppb98 <ppb98>
2023-01-11 15:36:32 +0200

    ff

--- resources/views/layouts/includes/head.blade.php
+++ resources/views/layouts/includes/head.blade.php
@@ -21,7 +21,6 @@
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-@stack('css')
 <style>
 @font-face {
     font-family: 'Cairo-Regular';
@@ -35,3 +34,4 @@ html, body{
     font-family: Cairo-Regular,"sans-serif" !important;
 
 }</style>
+@stack('css')
