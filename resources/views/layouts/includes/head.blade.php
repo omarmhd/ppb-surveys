224e13c2e18cb9d73b3dfb6be560c55b901b9a0b
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2023-01-12 12:54:58 +0200

    fsdf

--- resources/views/layouts/includes/head.blade.php
+++ resources/views/layouts/includes/head.blade.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <link rel="shortcut icon" href="{{asset("images/logo.png")}}">
 <!--begin::Fonts-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+{{--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>--}}
 <link rel="stylesheet" href="{{asset("assets/css/toastr.css")}}">
 {{--<link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/dark.css">--}}
 
